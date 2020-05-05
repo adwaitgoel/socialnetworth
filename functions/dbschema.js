dc467e1cc28c9de85bb548e22e022d2651d85c00
Adwait Goel <adwaitgoel@gmail.com>
2020-05-05 13:45:02 +0530

    First Commit - Auth handled

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is a body',
+            createdAt: "2020-05-04T09:28:07.929Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+
+    ]
+}
